1370c3b1e16eef7a2b8d04a7e5e2d98336c6bc7a
Alpha351-spec <2275199600@qq.com>
2026-07-20 19:06:22 +0800

    fix: 移除 build.gradle.kts 中 Windows 专用的 C:/b buildDir 配置，修复 Linux 构建失败

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,14 +5,6 @@ allprojects {
     }
 }
 
-// 将 build 目录改到 C 盘短路径，避免 CMake 在 G 盘上创建目录失败
-rootProject.layout.buildDirectory.set(file("C:/b"))
-val newBuildDir: Directory = rootProject.layout.buildDirectory.get()
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
 subprojects {
     project.evaluationDependsOn(":app")
 }
